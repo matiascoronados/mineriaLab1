e3ca1d240aed7c39dd9e52b727cbcb1521847fec
Matias <matias.coronado@usach.cl>
2021-04-27 13:49:58 -0400

    Avance 90%

--- lab1.r
+++ lab1.r
@@ -1,36 +1,43 @@
 #https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Prognostic)
-
 url.data.set <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/00329/messidor_features.arff'
 data.raw <- read.csv(url.data.set, header=FALSE, comment.char = "@")
 
+
+#Los datos representan los resultados obtenidos a partir del analisis de diferentes imagenes de retinas, entre las cuales
+#se encuentran casos activos de Retinopatía diabética (RN).
 df <- data.frame(data.raw)
 colnames(df) <- c(
-    "q",      #  0 The binary result of quality assessment. 0 = bad quality 1 = sufficient quality
-    "ps",     #  1 The binary result of pre-screening, 1 indicates severe retinal abnormality and 0 its lack
-    "nma.a",  #  2 Number of MAs found at the confidence levels alpha = 0.5
-    "nma.b",  #  3 Number of MAs found at the confidence levels alpha = 0.6
-    "nma.c",  #  4 Number of MAs found at the confidence levels alpha = 0.7
-    "nma.d",  #  5 Number of MAs found at the confidence levels alpha = 0.8
-    "nma.e",  #  6 Number of MAs found at the confidence levels alpha = 0.9
-    "nma.f",  #  7 Number of MAs found at the confidence levels alpha = 1.0
-    "nex.a",  #  8 Number of Exudates found at the confidence levels alpha = 0.5
-    "nex.b",  #  9 Number of Exudates found at the confidence levels alpha = 0.6
-    "nex.c",  # 10 Number of Exudates found at the confidence levels alpha = 0.7
-    "nex.d",  # 11 Number of Exudates found at the confidence levels alpha = 0.8
-    "nex.e",  # 12 Number of Exudates found at the confidence levels alpha = 0.9
-    "nex.g",  # 13 Number of Exudates found at the confidence levels alpha = 1.0
-    "nex.f",  # 14 Number of Exudates found at the confidence levels alpha = 1.0
-    "nex.h",  # 15 Number of Exudates found at the confidence levels alpha = 1.0
-    "dd",     # 16 The euclidean distance of the center of the macula and the center of the optic disc
-    "dm",     # 17 The diameter of the optic disc
-    "amfm",   # 18 The binary result of the AM/FM-based classification
-    "class"   # 19 Class label. 1 = contains signs of DR, 0 = no signs of DR
+    "q",      #  0 Numero binario, relacionado con la calidad de la imagen. 0 = Mala calidad; 1 = Buena calidad.
+    "ps",     #  1 Numero binario resultante de un pre-escaneo, que indica si la imagen presenta animalias graves en la retina.
+    "nma.a",  #  2 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 0.5
+    "nma.b",  #  3 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 0.6
+    "nma.c",  #  4 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 0.7
+    "nma.d",  #  5 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 0.8
+    "nma.e",  #  6 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 0.9
+    "nma.f",  #  7 Numero de MAs (microneurismas) encontrados en un nivel de confianza con alpha = 1.0
+    "nex.a",  #  8 Numero de Exudates encontrados en un nivel de confianza con alpha = 0.5
+    "nex.b",  #  9 Numero de Exudates encontrados en un nivel de confianza con alpha = 0.6
+    "nex.c",  # 10 Numero de Exudates encontrados en un nivel de confianza con alpha = 0.7
+    "nex.d",  # 11 Numero de Exudates encontrados en un nivel de confianza con alpha = 0.8
+    "nex.e",  # 12 Numero de Exudates encontrados en un nivel de confianza con alpha = 0.9
+    "nex.f",  # 13 Numero de Exudates encontrados en un nivel de confianza con alpha = 1.0
+    "nex.g",  # 14 Numero de Exudates encontrados en un nivel de confianza con alpha = 1.0
+    "nex.h",  # 15 Numero de Exudates encontrados en un nivel de confianza con alpha = 1.0
+    "dd",     # 16 Distancia eucladiana entre el centro de la macula y el centro del disco optico
+    "dm",     # 17 Diametro del disco optico
+    "amfm",   # 18 Numeri binario, relacionado con la clasificacion AM/FM-based
+    "class"   # 19 Clase del dato, en donde 1 = contiene signos de DR, 0 = no contiene signos de DR.
 )
 
-numericFeats = c(3:16)
-eyeFeats = c(17,18)
+#Se realiza una estanadarizacion de las observacion respecto a las varialbes. Esto se hace por separado en base a las 
+# dimenciones de las variables, para ser consecuente on los valores contemplados en las columnas.
+
+# Uno representa cantidad de exucdados y microneurismos encontrados.
+valoresCantidad = c(3:16)
+# El otro alude a la distancia eucladiana y el diametro del disco.
+valoresDistancia = c(17,18)
 df.normalizado <- df
-df.normalizado[, c(numericFeats, eyeFeats)] = scale(df[, c(numericFeats, eyeFeats)])
+df.normalizado[, c(valoresCantidad, valoresDistancia)] = scale(df[, c(valoresCantidad, valoresDistancia)])
 df.normalizado$class = as.factor(df.normalizado$class)
 
 ########### Determinar datos con baja calidad.
@@ -57,6 +64,23 @@ df$q <- as.numeric(df$q)
 df$ps <- as.numeric(df$ps)
 df$amfm <- as.numeric(df$amfm)
 df$class <- as.numeric(df$class)
+
+
+#Se realizara un analisis tomando en consideracion las duplas que contengan el mismo valor de alpha.
+datos.05 <- df[,-c(4,5,6,7,8,10,11,12,13,14,15,16)]
+
+datos.06 <- df[,-c(3,5,6,7,8,9,11,12,13,14,15,16)]
+
+datos.07 <- df[,-c(3,4,6,7,8,9,10,12,13,14,15,16)]
+
+datos.08 <- df[,-c(3,4,5,7,8,9,10,11,13,14,15,16)]
+
+datos.09 <- df[,-c(3,4,5,6,8,9,10,11,12,14,15,16)]
+
+datos.10 <- df[,-c(3,4,5,6,7,9,10,11,12,13,15,16)]
+
+
+
 ############ Medidas de centralizacion 
 # Media ; Mediana; 1Q ; 3Q; Max; Min
 summary(df)
@@ -65,7 +89,7 @@ summary(df)
 ############ Grafico de torta
 ## Create a frequency table
 df.table <- table(df$class)
-colors <- terrain.colors(2) 
+colors <- terrain.colors(2)
 
 # Create a pie chart 
 class.prop.table <- prop.table(df.table)*100
@@ -158,58 +182,70 @@ ggplot(df.normalizado, aes(x = nex.f, y = nma.f, color=class))+ facet_wrap(~amfm
 
 #No se ve ninguna relacion a simple vista; Es necesario ustilizar una agrupacion por modelo
 
+class <- df$class
+
+#Modelo con alpha 0.5
+data.sinclass.05 <- datos.05[,-20]
+BIC.05 <- mclustBIC(data.sinclass.05)
+summary(BIC.05)
+plot(BIC.05)
+# BIC = 2504.353
+
+#Modelo con alpha 0.6
+data.sinclass.06 <- datos.06[,-20]
+BIC.06 <- mclustBIC(data.sinclass.06)
+summary(BIC.06)
+plot(BIC.06)
+# BIC = 4926.973
+
+#Modelo con alpha 0.7
+data.sinclass.07 <- datos.07[,-20]
+BIC.07 <- mclustBIC(data.sinclass.07)
+summary(BIC.07)
+plot(BIC.07)
+# BIC = 6630.694 
+
+#Modelo con alpha 0.8
+data.sinclass.08 <- datos.08[,-20]
+BIC.08 <- mclustBIC(data.sinclass.08)
+summary(BIC.08)
+plot(BIC.08)
+# BIC = -5858.015
+
+#Modelo con alpha 0.9
+data.sinclass.09 <- datos.09[,-20]
+BIC.09 <- mclustBIC(data.sinclass.09)
+summary(BIC.09)
+plot(BIC.09)
+# BIC = -4651.728
+
+#Modelo con alpha 1.0
+data.sinclass.10 <- datos.10[,-20]
+BIC.10 <- mclustBIC(data.sinclass.10)
+summary(BIC.10)
+plot(BIC.10)
+# BIC = -2108.609
 
 
+#Tomando en consideracion los BIC obtenidos, se estima que la mejor opcione es 
+#alpha = 0.7, ya ahi el BIC alcanza su maximo valor.
 
-########## Codigo del profe.
-#corrplot(df, p.mat = res1$p, sig.level = .05)
-require(mclust)
+clPairs(data.sinclass.07, class)
 
+#Se procede a generar el modelo utilizando el set de tatos que maximiza el BIC.
 
-mod1 = Mclust(df[,1:19]) #DEFAULT 
-summary(mod1)
+modelo.07 = Mclust(data.sinclass.07, x = BIC.07)
+summary(modelo.07)
 
+#install.packages("factoextra")
+library(factoextra)
 
-mod3 = Mclust(df[,1:19], G = 10)  #Numero de grupos = 3.
-summary(mod3)
+#Grafico de la distribucion de los datos dentro de los clusters
+fviz_mclust(modelo.07, what = "classification", geom = "point",
+            pallete = "jco")
 
+#Ver como se estan agrupando los datos en relacion a la clasificacion anterior que se le dio a los grupos.
+plot(modelo.07, what = "classification")
 
-
-mod6 = Mclust(iris[,1:4], prior = priorControl(functionName="defaultPrior", shrinkage=0.1), modelNames ="EII")  
-#"EII" = spherical, equal volume # Using prior #The function priorControl is used to specify a conjugate prior for EM within MCLUST. 
-summary(mod6,parameter = TRUE)
-plot(mod6, what = "classification")
-
-class<-iris$Species
-
-BIC<-mclustBIC(iris[,1:4], prior = priorControl(functionName="defaultPrior", shrinkage=0.1))
-plot(BIC)  #se grafican los BIC por configuración de parámetros
-summary(BIC)  # se presentan los mejores valores BIC
-
-mod11=Mclust(iris[,1:4],x=BIC) # en base al mejor valor BIC se realiza el mclust
-summary(mod11)#se muestra resultado y tabla de clustering
-
-
-plot(mod11, what = "classification")  #se grafica la configuración de agrupamientos.
-legend("bottomright", legend = 1:2,
-       col = mclust.options("classPlotColors"),
-       pch = mclust.options("classPlotSymbols"),title = "Class labels:")
-
-table(class, mod11$classification) #distribución de clases por cada grupo.
-
-
-class<-iris$Species
-mod12 = Mclust(iris[,1:4], G=3, prior = priorControl(functionName="defaultPrior", shrinkage=0.1), modelNames ="VEV")  
-table(class, mod12$classification) #distribución de clases por cada grupo.
-summary(mod12)
-plot(mod12, what = "classification")  #se grafica la configuración de agrupamientos.
-legend("bottomright", legend = 1:3,
-       col = mclust.options("classPlotColors"),
-       pch = mclust.options("classPlotSymbols"),title = "Class labels:")
-
-# calculate collinearity
-corMatMy <- cor(bc_data[,2:31])
-corrplot(corMatMy, order = "hclust", tl.cex = 0.7)
-
-
-
+#Con esta tabla se conforman los valores de Sensibilidad y especificidad.
+table(class, modelo.07$classification)
